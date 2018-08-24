152aa658f8962b22f374915eb312796fae053777
Unknown <dillon.embreus@gmail.com>
2018-08-24 14:48:03 -0600

    Updated user profile page
    profile page now has a get request for a specific user and one for the other users

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,16 +1,50 @@
 import React, {Component, Fragment} from 'react'
 import NavBar from "./NavBar";
+// const getProfiles = async id => {
+//     fetch(`http://localhost:4000/${id}`, {})
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(myJson) {
+//         console.log(JSON.stringify(myJson));
+//     });
+// };
+export const getProfiles = async () => {
+    const response = await fetch('http://localhost:4000');
+    return await response.json()
+};
+const getProfile = async id =>
+
+    await fetch(`http://localhost:4000/D1DsZCGYFW76LlGI`, {})
+        .then(response => response.json());
+
 
 export default class UserProfile extends Component {
     state = {
-
+        profile: {},
+        profiles:[]
     };
+    async componentDidMount() {
+        try {
+            const id = this.props.match.params.id;
+            const profile = await getProfile(id);
+            this.setState({ profile });
+            const profiles = await getProfiles();
+            this.setState({profiles})
+        } catch(err) {
+            console.log(err)
+        }
+    }
     render(){
+        const profile = this.state.profile;
+        const profiles = this.state.profiles.filter(profile => profile._id !== this.state.profile._id);
         return(
             <Fragment className='user-profile'>
                 <NavBar/>
                 <h1>User Profile</h1>
-                <h2>{}</h2>
+                <h2>{`${profile.firstName} ${profile.lastName}`}</h2>
+                <h3>Other Users</h3>
+                <h4>{`${profiles.map(profile => (` ${profile.firstName} ${profile.lastName}`))}`}</h4>
             </Fragment>
         )
     }
