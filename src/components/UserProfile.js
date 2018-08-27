462b9a4042a25692912aba56b65110ab12e557d3
Unknown <dillon.embreus@gmail.com>
2018-08-27 12:48:15 -0600

    created delete user function
    implemented the function in the user profile account

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -9,42 +9,55 @@ import NavBar from "./NavBar";
 //         console.log(JSON.stringify(myJson));
 //     });
 // };
+
 export const getProfiles = async () => {
     const response = await fetch('http://localhost:4000');
     return await response.json()
 };
-const getProfile = async id =>
-
-    await fetch(`http://localhost:4000/D1DsZCGYFW76LlGI`, {})
+const getProfile = async id => {
+    return await fetch(`http://localhost:4000/${id}`, {})
         .then(response => response.json());
-
+};
+const deleteUser = async _id => {
+    return await fetch(`http://localhost:4000/${_id}`, {
+        method: 'delete'
+    }).then(response => {
+        response.json().then(json => {
+            return json;
+        })
+    });
+};
 
 export default class UserProfile extends Component {
     state = {
         profile: {},
-        profiles:[]
+        profiles: [],
+        _id: "D1DsZCGYFW76LlGI"
     };
+
     async componentDidMount() {
         try {
             const id = this.props.match.params.id;
-            const profile = await getProfile(id);
-            this.setState({ profile });
+            const profile = await getProfile(this.state._id);
+            this.setState({profile});
             const profiles = await getProfiles();
             this.setState({profiles})
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
-    render(){
+
+    render() {
         const profile = this.state.profile;
         const profiles = this.state.profiles.filter(profile => profile._id !== this.state.profile._id);
-        return(
+        return (
             <Fragment className='user-profile'>
                 <NavBar/>
                 <h1>User Profile</h1>
                 <h2>{`${profile.firstName} ${profile.lastName}`}</h2>
                 <h3>Other Users</h3>
                 <h4>{`${profiles.map(profile => (` ${profile.firstName} ${profile.lastName}`))}`}</h4>
+                <button className="btn btn-danger" onClick={deleteUser(this.state._id)}>Delete Profile</button>
             </Fragment>
         )
     }
---
+++ src/components/deleteUser.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+const deleteUser = async _id => {
+    return await fetch(`http://localhost:4000/${_id}`, {
+        method: 'delete'
+    }).then(response => {
+        response.json().then(json => {
+            return json;
+        })
+    });
+};
+export default deleteUser();
